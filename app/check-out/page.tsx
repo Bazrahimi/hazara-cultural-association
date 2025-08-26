09c970a5586de66bdec73325e662ccd2f772836b
Baz Rahimi <bazrahimi@hotmail.com>
2025-08-26 14:32:35 +1000

    add

---
+++ app/check-out/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CheckoutButton } from '../ui/stripe/CheckoutButton'
+
+const page = () => {
+  return (
+    <div>
+      Check out page
+      <CheckoutButton />
+    </div>
+
+  )
+}
+
+export default page
---
+++ app/ui/stripe/CheckoutButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+import { ActionButton } from "../global/clientComponent";
+
+export const CheckoutButton = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleCheckout = async () => {
+    setLoading(true);
+    const res = await fetch("/api/checkout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        items: [{ name: "sample Product", price: 2000, quantity: 1 }],
+      }),
+    });
+    const data = await res.json();
+
+    if (data.url) {
+      window.location.href = data.url;
+    }
+    setLoading(false);
+  };
+
+  return (
+    <ActionButton isLoading={loading} loadingText="Checkout" onClick={handleCheckout}>
+      Checkout
+    </ActionButton>
+  );
+};
