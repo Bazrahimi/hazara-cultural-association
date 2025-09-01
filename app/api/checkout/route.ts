8e31f3d23b3fbbf877fb090f5736e6a3ab8cdfcb
Baz Rahimi <bazrahimi@hotmail.com>
2025-09-01 11:16:38 +1000

    add

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,14 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+import { stripe } from "@/app/lib/stripe";
+
+export const POST = async(req:Request) => {
+  try {
+    
+    
+  } catch (error) {
+    
+  }
+
+
+}
