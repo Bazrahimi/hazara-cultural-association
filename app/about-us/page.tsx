4a34abe3b49d8e0db87a5e0047ade592d393e270
Baz Rahimi <bazrahimi@hotmail.com>
2025-09-28 23:26:00 +1000

    added phone numbers

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -9,41 +9,62 @@ const MANAGEMENT: Person[] = [
     role: "President",
     img: "/images/member/batoor.png",
     bio: "An award-winning photojournalist and community leader, Batoor brings years of advocacy experience to HCA. He is committed to preserving Hazara heritage and amplifying the voices of his community.",
-    contact: "+61 469 813 842"
+    contact: "+61 469 813 842",
   },
   {
     name: "Yasin Hazara",
     role: "Vice President | Senior Public Liaison officer",
     img: "/images/member/yasin.png",
+    contact: "+61 423 536 719",
   },
-  { name: "Zabih Rezai", role: "Secretary", img: "/images/member/zabi.png" },
+  {
+    name: "Zabih Rezai",
+    role: "Secretary",
+    img: "/images/member/zabi.png",
+    contact: "+61 412 984 700",
+  },
   {
     name: "Liaquat Ali",
     role: "Treasurer",
     img: "/images/member/Liaquat.jpeg",
+    contact: "+61 420 910 786",
   },
 
   {
     name: "Ali Khan",
     role: "Policy & Strategic Advisor",
     img: "/images/member/khan.png",
+    contact: "+61 401 824 640",
   },
   {
-  name: "Baz Rahimi",
-  role: "Governance Advisor | Full Stack Web Developer",
-  img: "/images/member/rahimi.jpg",
-},
+    name: "Baz Rahimi",
+    role: "Governance Advisor | Full Stack Web Developer",
+    img: "/images/member/rahimi.jpg",
+  },
 
   {
     name: "Latif Mahmoodi",
     role: "Assistant Public Liaison officer",
     img: "/images/member/latif.png",
+    contact: "+61 450 108 322",
   },
 
-  { name: "Jawid Ahmad Nairan", img: "/images/member/nairan.png" },
+  {
+    name: "Jawid Ahmad Nairan",
+    img: "/images/member/nairan.png",
+    contact: "+61 468 855 007",
+  },
 
-  { name: "Abulfazl Zaki", img: "/images/member/abul.png" },
-  { name: "Ahmad Reza Fayaz", img: "/images/member/fayaz.png" },
+  {
+    name: "Abulfazl Zaki",
+    img: "/images/member/abul.png",
+    contact: "+61 404 333 764",
+  },
+  {
+    name: "Ahmad Reza Fayaz",
+    img: "/images/member/fayaz.png",
+    contact: "+61 482 668 596",
+  },
 ];
 
 export default function AboutUsPage() {
