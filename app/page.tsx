8a3d2c6dd26ea314bd9ca7cf501666ddb10dd5b9
Baz Rahimi <bazrahimi@hotmail.com>
2025-09-30 00:33:30 +1000

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Memorial133 from "./hazara-genocide-memorial/133/page";
 import PartnersAndSupporters from "./partners/page";
 import Hero from "./ui/Hero";
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <div>
       <Hero />
+      <Memorial133 />
       <PartnersAndSupporters />
     </div>
   );
