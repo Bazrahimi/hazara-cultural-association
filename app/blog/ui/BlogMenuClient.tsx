a93826bbe372bcd114bdffef73bce3d6cce60c7e
Baz Rahimi <bazrahimi@hotmail.com>
2025-11-27 17:47:58 +1100

    add

--- app/blog/ui/BlogMenuClient.tsx
+++ app/blog/ui/BlogMenuClient.tsx
@@ -12,8 +12,9 @@ const baseItems = [
   ...catItems,
   {
     label: "Hazara Genocide Memorial",
-    href: "blog/hazara-genocide-memorial/133",
+    href: "/blog/hazara-genocide-memorial/133",
   },
+  // TODO: using this spread method add new new object does not come with bugs. when user on hte blog. if they click on hazara genocide memorial. it i will append end point to existing end point
 ];
 
 const manageItems = [
