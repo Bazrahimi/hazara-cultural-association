c0800749d2418b825dada140f1a2f111d55091f2
Baz Rahimi <bazrahimi@hotmail.com>
2025-12-30 00:56:54 +1100

    add

--- app/blog/ui/posts/PostCard.tsx
+++ app/blog/ui/posts/PostCard.tsx
@@ -26,7 +26,7 @@ const PostCard = ({ post }: PostCardProps) => {
       href={`${BlogRoutes.post(post.slug)}?catId=${post.categoryId}&rtl=${isRTL ? 1 : 0}&id=${post.postId}`}
       className="group overflow-hidden rounded-xl border border-gray-100 bg-white shadow-sm transition hover:shadow-md"
     >
-      <article className="flex h-full flex-col">
+      <article className="flex h-full flex-col p-1">
         {/* Top: title*/}
 
         <Header
